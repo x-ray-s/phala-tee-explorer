4923abb0d74b4d11d0b92332fea1bf5c74f844a1
ray <pricesswar721@gmail.com>
2024-09-30 17:34:50 +0800

    fix: remove cdn to fix useSyncExternalStore error

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import cdn from 'vite-plugin-cdn-import'
 import * as path from 'path'
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        cdn({
-            modules: ['react', 'react-dom'],
-        }),
         react({
             jsxRuntime: 'automatic',
         }),
